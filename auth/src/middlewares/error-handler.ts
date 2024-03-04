41d0a6a721375cd6b9c7eac844166266e59dbeb5
Bash-Clevin <clevinbash@gmail.com>
2024-03-04 08:28:40 +0300

    feat(auth): add error handler custom errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +8,19 @@ export const errorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  console.log('Something went wrong');
+  if (err instanceof RequestValidationError) {
+    const formattedError = err.errors.map((error) => {
+      return { message: error.msg, field: error.type };
+    });
+
+    return res.status(400).send({ errors: formattedError });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: err.message }],
   });
 };

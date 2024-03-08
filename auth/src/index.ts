572e7b04b67c585951aaf832eeab0d6523db841b
Bash-Clevin <clevinbash@gmail.com>
2024-03-08 22:23:03 +0300

    chore(auth): use dynamic db urls

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,9 +5,14 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log('Connected to mongo db');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to auth db');
   } catch (error) {
     console.log(error);
   }

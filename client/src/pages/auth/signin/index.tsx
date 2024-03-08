d975c2f50950608f8c2f175be02abf1a8a8d34b4
Bash-Clevin <clevinbash@gmail.com>
2024-03-08 14:13:33 +0300

    chore(client): Add signin route

---
+++ client/src/pages/auth/signin/index.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import useRequest from '@/hooks/use-request';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
+
+export default function Signin() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const router = useRouter();
+
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => router.push('/'),
+  });
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign In</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          type="text"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button type="submit" className="btn btn-primary">
+        Sign In
+      </button>
+    </form>
+  );
+}

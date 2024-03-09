30d115c9bfeb0dd1aa7a65a41b4241bf955853c5
Bash-Clevin <clevinbash@gmail.com>
2024-03-09 09:24:56 +0300

    chore(tickets): show ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import 'express-async-errors';
 import { NotFoundError, errorHandler, currentUser } from '@clevinbash/library';
 import { createTicketRouter } from './routes/new-ticket';
+import { showTicketRouter } from './routes/show-ticket';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,6 +19,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();

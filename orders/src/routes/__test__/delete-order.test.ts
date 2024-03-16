45bc8a5dfb11658aee6759889a93be6b19e023b6
Bash-Clevin <clevinbash@gmail.com>
2024-03-16 08:40:24 +0300

    feat(orders): implement delete order and tests

---
+++ orders/src/routes/__test__/delete-order.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import { Ticket } from '../../models/ticket';
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+
+it('marks an order as cancelled', async () => {
+  // create ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // create an order
+  const userToken = global.signin();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userToken)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // cancel order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', userToken)
+    .send()
+    .expect(204);
+
+  // make sure order is cancelled in db
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('should emit order cancelled event');

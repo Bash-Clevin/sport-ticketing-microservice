0de2598822b5e9cd21efb141b50414465f5db132
Bash-Clevin <clevinbash@gmail.com>
2024-03-16 09:35:30 +0300

    chore(orders): add event publishing tests

--- orders/src/routes/__test__/delete-order.test.ts
+++ orders/src/routes/__test__/delete-order.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { Ticket } from '../../models/ticket';
 import { app } from '../../app';
 import { Order, OrderStatus } from '../../models/order';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('marks an order as cancelled', async () => {
   // create ticket
@@ -33,4 +34,28 @@ it('marks an order as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo('should emit order cancelled event');
+it('should emit order cancelled event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // create an order
+  const userToken = global.signin();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userToken)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // cancel order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', userToken)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});

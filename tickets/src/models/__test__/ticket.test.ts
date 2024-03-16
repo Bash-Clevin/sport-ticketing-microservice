7620f1951590b3be5ba956bcc46e47f45b4aa1ed
Bash-Clevin <clevinbash@gmail.com>
2024-03-16 12:38:49 +0300

    feat(tickets): test if ticket version is incrementing

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -20,3 +20,16 @@ it('implements optimistic concurrency control', async () => {
   // expect an error to be thrown when saving
   await expect(secondInstance!.save()).rejects.toThrow();
 });
+
+it('increments the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'test ticket',
+    price: 5,
+    userId: '123',
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+});

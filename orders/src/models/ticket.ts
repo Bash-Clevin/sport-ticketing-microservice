01f59c8f4fa364f0077d7af2c1fccb334d71a3ec
Bash-Clevin <clevinbash@gmail.com>
2024-03-16 14:47:27 +0300

    chore(orders): remove plugin and use presave

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 import { Order, OrderStatus } from './order';
-import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttributes {
   id: string;
@@ -48,7 +47,13 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.set('versionKey', 'version');
-ticketSchema.plugin(updateIfCurrentPlugin);
+
+ticketSchema.pre('save', function (done) {
+  this.$where = {
+    version: this.get('version') - 1,
+  };
+  done();
+});
 
 ticketSchema.statics.build = (attributes: TicketAttributes) => {
   return new Ticket({

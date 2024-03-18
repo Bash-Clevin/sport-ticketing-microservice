e3cce68fc12083f31c4e2c4605e9467dd9f4ab5a
Bash-Clevin <clevinbash@gmail.com>
2024-03-18 07:46:15 +0300

    feat(tickets): check if ticket is reserved

--- tickets/src/routes/__test__/update-ticket.test.ts
+++ tickets/src/routes/__test__/update-ticket.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 import { natsWrapper } from '../../nats-wrapper';
+import { Ticket } from '../../models/tickets';
 
 it('returns a 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -128,4 +129,32 @@ it('publishes an event', async () => {
     .expect(200);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('should reject editing on reserved tickets', async () => {
+  const cookie = global.signin();
+  const title = 'test title';
+  const price = 120;
+
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'abcdefgh',
+      price: 10,
+    });
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() });
+
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title,
+      price,
+    })
+    .expect(400);
 });
